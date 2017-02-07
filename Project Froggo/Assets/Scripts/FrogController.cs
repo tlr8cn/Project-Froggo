2d1d3af1b64047b6d13c993e97091935aa679871
Tyler Robinson <tlr8cn@virginia.edu>
2017-02-07 17:55:44 -0500

    tree frog movement and camera movement

--- Project Froggo/Assets/Scripts/FrogController.cs
+++ Project Froggo/Assets/Scripts/FrogController.cs
@@ -4,41 +4,65 @@ using UnityEngine;
 
 public class FrogController : MonoBehaviour {
 
+	public float jumpSpeed;
+	public float jumpProgress;
+	public bool jumping;
+
+	float jumpMax = 2.56f;
+
+	Animator anim;
 
 
 	// Use this for initialization
 	void Start () {
-		
+		jumping = false;
+
+		anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+		anim.SetBool("Jumping", jumping);
 
-	void FixedUpdate() {
+		if(jumping) {
+			transform.Translate(jumpSpeed*Vector3.up);
+			jumpProgress += jumpSpeed;
+			if(jumpProgress == jumpMax) {
+				jumping = false;
+			}
+		}
+
+
 		//Up
-		if (Input.GetKeyDown(KeyCode.W)) {
+		if (Input.GetKeyDown(KeyCode.W) && !jumping) {
+			jumping = true;
+			jumpProgress = 0f;
 			transform.eulerAngles = new Vector3(0f, 0f, 0f);
-			transform.Translate(2.56f*Vector3.up);
 		}
 
 		//Down
-		if (Input.GetKeyDown(KeyCode.S)) {
+		if (Input.GetKeyDown(KeyCode.S) && !jumping) {
+			jumping = true;
+			jumpProgress = 0f;
 			transform.eulerAngles = new Vector3(0f, 0f, 180f);
-			transform.Translate(2.56f*Vector3.up);
 		}
 
 		//Left
-		if (Input.GetKeyDown(KeyCode.A)) {
+		if (Input.GetKeyDown(KeyCode.A) && !jumping) {
+			jumping = true;
+			jumpProgress = 0f;
 			transform.eulerAngles = new Vector3(0f, 0f, 90f);
-			transform.Translate(2.56f*Vector3.up);
 		}
 
 		//Right
-		if (Input.GetKeyDown(KeyCode.D)) {
+		if (Input.GetKeyDown(KeyCode.D) && !jumping) {	
+			jumping = true;
+			jumpProgress = 0f;
 			transform.eulerAngles = new Vector3(0f, 0f, -90f);
-			transform.Translate(2.56f*Vector3.up);
 		}
+	}
+
+	void FixedUpdate() {
+
 	}
 }
